a8fe41f1304643c9be7d026ba0ec11936ec7d474
dishant1123 <dishantshah1417@gmail.com>
2026-01-05 16:55:55 +0530

    ....

--- 5jan.py
+++ 5jan.py
@@ -1,8 +1,10 @@
 import numpy as np 
-
-# arange   ==> start  , stop  , step 
-# ones ==> 1  ==>default    ==>float  
-#zeros  ==> 0  ==>default    ==>float
+# intrinsic method  : 
+"""
+arange   ==> start  , stop  , step 
+ones ==> 1  ==>default    ==>float  
+zeros  ==> 0  ==>default    ==>float
+"""
 """
 a=np.arange(1,10,2)
 print(a)
@@ -25,3 +27,26 @@ print(d)
  1 0 0 0 1
  1 1 1 1 1
 """
+
+"""e=np.full((3,4),44) 
+print(e)
+
+a=np.array([
+    [1,2,3,4,5,6,7],
+    [8,9,10,11,12,13,14],
+    [15,16,17,18,19,20,21],
+    [22,23,24,25,26,27,28]
+])
+
+b= np.full_like(a,10)
+print(b)
+"""
+import random 
+
+"""a =np.random.randint(low=-10,high =10,size=12).reshape(3,4)
+a =np.random.randint(1,4,size=(2,2))  # high  excluded 
+print(a)
+"""
+
+b=np.random.random(10).reshape(2,5)
+print(b)
