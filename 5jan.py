0bd14ad7ba8f944e9117764796c54dc391552ca6
dishant1123 <dishantshah1417@gmail.com>
2026-01-05 16:31:43 +0530

    ....

---
+++ 5jan.py
@@ -0,0 +1,27 @@
+import numpy as np 
+
+# arange   ==> start  , stop  , step 
+# ones ==> 1  ==>default    ==>float  
+#zeros  ==> 0  ==>default    ==>float
+"""
+a=np.arange(1,10,2)
+print(a)
+
+b=np.arange(1,13).reshape(4,3)
+print(b)
+
+c=np.ones(12,dtype=int).reshape(3,4)
+print(c)
+
+d=np.zeros(12,dtype=int).reshape(3,4)
+print(d)
+"""
+
+# task  :1 
+"""
+ 1 1 1 1 1 
+ 1 0 0 0 1 
+ 1 0 9 0 1 
+ 1 0 0 0 1
+ 1 1 1 1 1
+"""
