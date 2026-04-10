d8a9d52fd8fbbeb839d3e1caf7c21e7b3d421478
dishant1123 <dishantshah1417@gmail.com>
2026-04-10 17:18:38 +0530

    ....

---
+++ pandas/10april.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+movies = pd.read_csv("pandas\movies.csv")
+directors = pd.read_csv("pandas\directors.csv")
+
+# print(movies)
+print(directors)
+
+# task  :1 remove  first  col  in both data  movies and directors  :
+# task  :2 join  ==> director_id join 
