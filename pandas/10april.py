9ecf7db579e841943c649f0d110abe09416254f8
dishant1123 <dishantshah1417@gmail.com>
2026-04-17 17:25:05 +0530

    ....

--- pandas/10april.py
+++ pandas/10april.py
@@ -32,7 +32,7 @@ df = movies.merge(directors, left_on='director_id', right_on='id',how='inner')
 
 # top 10 director name by movies :
 
-top_10_directors = df.groupby('director_name').size().nlargest(10)
+"""top_10_directors = df.groupby('director_name').size().nlargest(10)
 print(top_10_directors)
 
 plt.bar(top_10_directors.index, top_10_directors.values, color='red', align='center', width=0.5, edgecolor='black', linewidth=1, label='Top 10 Directors by Movies')
@@ -40,5 +40,27 @@ plt.xlabel('Director Name')
 plt.ylabel('Number of Movies')
 plt.title('Top 10 Directors by Movies')
 plt.show()
-
+"""
 # top 10 movies by revenue : 
+
+"""days=[1,2,3,4,5,6,7,8,9,10]  # days 
+temparature=[35,37,38.90,29,31,30,27,26,25,24]
+
+plt.plot(days,temparature,color = 'red',label = 'temperature',marker = 'o',linewidth = 2,markersize = 6,linestyle = '-',markeredgecolor = 'black')
+plt.xlabel('Days')
+plt.ylabel('Temparature')
+plt.title('Temparature')
+plt.show()
+"""
+
+marks = [100,99,78,89,50,88,67]
+name =["dev","sudev","het","jay","ram","sita","priya"]
+
+plt.bar(name,marks,color="red",align="center",width=0.5,edgecolor="black",linewidth=1,label="marks")
+plt.xlabel("name")
+plt.ylabel("marks")
+plt.title("marks of students")
+plt.xticks(rotation=90,ha="right",fontsize=10)
+plt.yticks(rotation=80,ha="right",fontsize=10)
+plt.show()
+
